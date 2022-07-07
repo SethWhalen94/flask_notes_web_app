3d7190ef1121f9905a01ea844f9868f8f81a8c6a
SethWhalen94 <whalen_designco@outlook.com>
2022-07-06 20:50:21 -0600

    add code for models and db creation. Remove user checks in HTML Jinja to avoid undefined errors. This will be added back when USer authentication is implemented

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'aswefhnewufbweibf43ui4'  # Create secret key for session cookies
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(DB_NAME)  # Register URI for database in app config
+    db.init_app(app) # Initialize database to be used with our app
 
     from .views import views
     from .auth import auth
